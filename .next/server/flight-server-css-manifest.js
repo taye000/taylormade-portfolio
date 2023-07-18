3f1ad0ae80944c434dec94cdb54ed94417e968a9
taye000 <taylorgitari@gmail.com>
2023-07-18 13:50:08 +0200

    setup

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/home/taye/dev/next/taylormade-portfolio/app/page": [
+      "/home/taye/dev/next/taylormade-portfolio/styles/globals.css"
+    ]
+  },
+  "/home/taye/dev/next/taylormade-portfolio/app/layout.tsx": [
+    "/home/taye/dev/next/taylormade-portfolio/styles/globals.css"
+  ]
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-2xl">Taylormade</h1>
+    </main>
+  );
+}
